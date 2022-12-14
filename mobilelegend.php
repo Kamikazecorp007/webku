909dd2e60bbc4e355bae8ca1bfcc44b5bb665113
Kamikazecorp007 <tataultras@gmail.com>
2022-12-14 17:00:07 +0700

    hmm

---
+++ mobilelegend.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mobile Legend</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    <link rel="stylesheet" href="mobilelegend.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="index.php"> <img src="T.png" width="100PX"> </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        </div>
+      </nav>
+
+    <div class="wrapper">
+
+                <section id="MASUK"></section>
+
+                <nav style="padding-left: 120px;">
+                   
+                           
+                        </div>
+                    </div>
+                </nav>
+
+                <div class="row mt-5">
+                    <div class="col-md-1"></div>
+                    <div class="col-md-10">
+                      <div class="card" style="width: 90%">
+                          <div class="card-header">Pembelian Diamond Mobile Legend</div>
+                          <div class="card-body">
+                            <div class="card-header bg-white" >Masukan ID dan SERVER</div>
+                             <div class="card-body">
+                                <input type="text" id="nomer" class="form-control" name="nomer" placeholder="ID dan SERVER">
+                             </div>
+                             <div class="mt-3">
+                                     <div class="card-header bg-white">Pilih Produk</div>
+                             </div>
+                                <div id="list-produk">
+                                    <div class="accordion" id="accordionExample">
+                                        <div class="card">
+                                            <div class="card-header" id="heading1">
+                                                <h2 class="mb-0">
+                                                    <button class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapse1" aria-expanded="false" aria-control="1">Umum</button>
+                                                </h2>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                              <div class="row">
+                                 
+                              </div>
+                          </div>
+                        </div>
+                <div class="card">
+                    <div class="card-header bg-white">Petunjuk</div>
+                    <div class="card-body">
+                        <p>Pesan Diamond Mobile Legend Masukkan id dan server Contoh Id =12345678 , Server= 1234 Contoh format = 123456781234, Tidak Ada Pengembalian Dana Untuk Kesalahan Pengguna Yang Pesanannya Sudah Terlajur Di Pesan.</p>
+                    </div>
+                </div>
+
+
+                  
+                    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+    <link rel="stylesheet" href="cssregister.css">
+</head>
+<body>
+<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600' rel='stylesheet' type='text/css'>
+<link href="//netdna.bootstrapcdn.com/font-awesome/3.1.1/css/font-awesome.css" rel="stylesheet">
+
+<div class="testbox">
+  <h1>DAFTAR GOOGLE ACCOUNT</h1>
+
+  <form action="/">
+    
+   
+  
+    <hr>
+
+    <input type="text" name="name" id="name" placeholder="Email" required/>
+  
+    <input type="text" name="name" id="name" placeholder="Name" required/>
+  
+    <input type="password" name="name" id="name" placeholder="Password" required/>
+  
+  
+   <p>By clicking Register, you agree on our <a href="#">terms and condition</a>.</p>
+   <a href="#" class="button">Register</a>
+  </form>
+</div>
+    
+</body>
+</html>
